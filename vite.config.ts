6dc30ff9fb55205eed472374ee0826d2c1e8e702
Paul Matthew Lazarte <112068330+talosren@users.noreply.github.com>
2025-01-23 18:47:11 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,10 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+  },
 });
